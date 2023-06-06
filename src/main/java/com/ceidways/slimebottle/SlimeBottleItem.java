5de9aa16673977b963effde5ab8712be034acf79
ceidways <ceidways@gmail.com>
2023-06-06 11:51:16 +1000

    Added item, recipe, obtainable by right click on slime, extra nausea functionality.

---
+++ src/main/java/com/ceidways/slimebottle/SlimeBottleItem.java
@@ -0,0 +1,75 @@
+package com.ceidways.slimebottle;
+
+import net.minecraft.advancement.criterion.Criteria;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemUsage;
+import net.minecraft.item.Items;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.stat.Stats;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.util.UseAction;
+import net.minecraft.world.World;
+
+public class SlimeBottleItem extends Item {
+    private static final int MAX_USE_TIME = 120; //honey bottle is 40
+    private static final int NAUSEA_DELAY = 6;
+    public SlimeBottleItem(Item.Settings settings) {
+        super(settings);
+    }
+
+    public ItemStack finishUsing(ItemStack stack, World world, LivingEntity user) {
+        super.finishUsing(stack, world, user);
+        if (user instanceof ServerPlayerEntity serverPlayerEntity) {
+            Criteria.CONSUME_ITEM.trigger(serverPlayerEntity, stack);
+            serverPlayerEntity.incrementStat(Stats.USED.getOrCreateStat(this));
+        }
+
+        if (stack.isEmpty()) {
+            return new ItemStack(Items.GLASS_BOTTLE);
+        } else {
+            if (user instanceof PlayerEntity && !((PlayerEntity)user).getAbilities().creativeMode) {
+                ItemStack itemStack = new ItemStack(Items.GLASS_BOTTLE);
+                PlayerEntity playerEntity = (PlayerEntity)user;
+                if (!playerEntity.getInventory().insertStack(itemStack)) {
+                    playerEntity.dropItem(itemStack, false);
+                }
+            }
+
+            return stack;
+        }
+    }
+
+    public int getMaxUseTime(ItemStack stack) {
+        return MAX_USE_TIME;
+    }
+
+    public UseAction getUseAction(ItemStack stack) {
+        return UseAction.DRINK;
+    }
+
+    public SoundEvent getDrinkSound() {
+        return SoundEvents.ITEM_HONEY_BOTTLE_DRINK;
+    }
+
+    public SoundEvent getEatSound() {
+        return SoundEvents.ENTITY_SLIME_SQUISH;
+    }
+
+    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
+        return ItemUsage.consumeHeldItem(world, user, hand);
+    }
+
+    public void usageTick(World world, LivingEntity user, ItemStack stack, int remainingUseTicks) {
+        if (MAX_USE_TIME - NAUSEA_DELAY - remainingUseTicks >= 1) {
+            user.addStatusEffect(new StatusEffectInstance(StatusEffects.NAUSEA, MAX_USE_TIME + 60 - NAUSEA_DELAY - remainingUseTicks, -1));
+        }
+    }
+}
